e083994f872a18c596fff8e4dde370a74a210574
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-04-24 14:15:02 -0400

    add image_diff

--- aibox/torch/image.py
+++ aibox/torch/image.py
@@ -1,5 +1,6 @@
 try:
     import torch
+    from torchvision.transforms import ToTensor
 except ImportError:
     print("pytorch required for these utilities")
     exit(1)
@@ -8,6 +9,7 @@ from typing import Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage.util import compare_images
 
 from .transforms import TensorImageToNumpy
 
@@ -19,6 +21,16 @@ def show_tensor_image(image: torch.Tensor):
     plt.imshow(reverseTransforms(image))
 
 
+def image_diff(image1: torch.Tensor | np.ndarray, image2: torch.Tensor | np.ndarray, return_tensor=True):
+    to_numpy = TensorImageToNumpy()
+    i1 = to_numpy(image1) if isinstance(image1, torch.Tensor) else image1
+    i2 = to_numpy(image2) if isinstance(image2, torch.Tensor) else image2
+    comp = compare_images(i1, i2, method="diff")
+    if return_tensor:
+        return ToTensor()(comp)
+    return comp
+
+
 def calc_shape_2d_conv(
     H_in,
     W_in,
@@ -27,14 +39,8 @@ def calc_shape_2d_conv(
     dilation=(1, 1),
     kernel_size=(3, 3),
 ):
-    H_out = np.floor(
-        (H_in + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) / (stride[0])
-        + 1
-    )
-    W_out = np.floor(
-        (W_in + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1) / (stride[1])
-        + 1
-    )
+    H_out = np.floor((H_in + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) / (stride[0]) + 1)
+    W_out = np.floor((W_in + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1) / (stride[1]) + 1)
     return H_out, W_out
 
 
@@ -47,20 +53,8 @@ def calc_shape_2d_transpose(
     kernel_size=(3, 3),
     output_padding=(1, 1),
 ):
-    H_out = (
-        (H_in - 1) * stride[0]
-        - 2 * padding[0]
-        + dilation[0] * (kernel_size[0] - 1)
-        + output_padding[0]
-        + 1
-    )
-    W_out = (
-        (W_in - 1) * stride[1]
-        - 2 * padding[1]
-        + dilation[1] * (kernel_size[1] - 1)
-        + output_padding[1]
-        + 1
-    )
+    H_out = (H_in - 1) * stride[0] - 2 * padding[0] + dilation[0] * (kernel_size[0] - 1) + output_padding[0] + 1
+    W_out = (W_in - 1) * stride[1] - 2 * padding[1] + dilation[1] * (kernel_size[1] - 1) + output_padding[1] + 1
     return H_out, W_out
 
 
