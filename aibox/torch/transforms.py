70f41dc634cbdac4aaf1de7f037a5ddb2f79c66a
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-05-01 08:33:19 -0400

    refactor transform name

--- aibox/torch/transforms.py
+++ aibox/torch/transforms.py
@@ -8,26 +8,13 @@ except ImportError:
 import numpy as np
 
 
-class TensorImageToNumpy:
-    def __init__(self, denorm=True, mode="RGB"):
+class ToNumpyImage:
+    def __init__(self, mode="RGB"):
         """Transforms a CHW tensor to numpy image.
 
         Args:
             denorm (bool, optional): Flag to denormalize image. Assumes _. Defaults to True.
         """
-        # if denorm:
-        #     transforms = [Lambda(lambda t: (t + 1) / 2)]
-        # else:
-        #     transforms = []
-
-        # transforms.extend(
-        #     [
-        #         Lambda(lambda t: t.permute(1, 2, 0)),  # CHW to HWC
-        #         Lambda(lambda t: t * 255.0),
-        #         Lambda(lambda t: t.cpu().detach().numpy().astype(np.uint8)),
-        #     ]
-        # )
-
         self.transform = Compose([ToPILImage(mode=mode), Lambda(lambda img: np.array(img))])
 
     def __call__(self, image: torch.Tensor):
