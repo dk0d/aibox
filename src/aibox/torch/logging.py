fb046fb2f6df6fb28ca2d750b414e489a07ceeb2
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-05-15 11:32:23 -0400

    fix: remove tensorboardX dep

--- src/aibox/torch/logging.py
+++ src/aibox/torch/logging.py
@@ -10,8 +10,8 @@ try:
     import mlflow
 
     from mlflow.client import MlflowClient
-    from pytorch_lightning.loggers import MLFlowLogger
-    from tensorboardX import SummaryWriter
+    from pytorch_lightning.loggers import MLFlowLogger, TensorBoardLogger
+    from torch.utils.tensorboard.writer import SummaryWriter
 except ImportError:
     print("MLFlow not installed, CombinedLogger will not be available.")
     mlflow = None
@@ -38,18 +38,32 @@ if mlflow is not None:
 
         @property
         def tb_writer(self) -> SummaryWriter:
-            return self.writer
+            return self._tb_logger.experiment
 
         @property
         def log_dir(self) -> str:
             return self.experiment.tracking_uri
 
-        def __init__(self, tensorboard_logdir=None, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            tensorboard_logdir = tensorboard_logdir or Path("logs")
-            self.writer = SummaryWriter(
-                logdir=(tensorboard_logdir / self.experiment_id / self.run_id).expanduser().resolve().as_posix()
+        def __init__(self, root_log_dir=None, **kwargs):
+            self.root_log_dir = Path(root_log_dir or "logs")
+            tensorboard_logdir = self.root_log_dir / "tbruns"
+            if "tracking_uri" not in kwargs:
+                mlflow_logdir = f"file:{self.root_log_dir / 'mlruns'}"
+                kwargs.update(tracking_uri=mlflow_logdir)
+
+            super().__init__(**kwargs)
+
+            self._tb_logger = TensorBoardLogger(
+                tensorboard_logdir.expanduser().resolve().as_posix(),
+                name=self.experiment_id,
+                version=self.run_id,
+                log_graph=True,
             )
+
+            # self.writer = SummaryWriter(
+            #     logdir=(tensorboard_logdir / self.experiment_id / self.run_id).expanduser().resolve().as_posix(),
+            #     log_graph=True,
+            # )
 
         def log_hyperparams(self, params: dict[str, Any] | Namespace | DictConfig) -> None:
             if isinstance(params, DictConfig):
@@ -58,3 +72,6 @@ if mlflow is not None:
                 self.experiment.log_dict(self.run_id, container, "config.yml")
                 params = dict(**config_to_dotlist(params))
             super().log_hyperparams(params)
+
+        def log_graph(self, *args, **kwargs):
+            return self._tb_logger.log_graph(*args, **kwargs)
