219b1ebb79ad2a7d4672babc00b065f904bfb995
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-05-17 12:26:20 -0400

    feat: add print fn combining rich and pprint

--- src/aibox/utils.py
+++ src/aibox/utils.py
@@ -1,1 +1,7 @@
+from pprint import pformat
+from rich import print as rprint
 
+
+def print(*args, **kwargs):
+    args = [a if isinstance(a, str) else pformat(a) for a in args]
+    rprint(*args, **kwargs)
