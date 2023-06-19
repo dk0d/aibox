6a3c78e5f89157a3ca085a0933e0c1eb989d2549
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-06-19 15:02:02 -0400

    format: utils.py

--- src/aibox/torch/utils.py
+++ src/aibox/torch/utils.py
@@ -1,5 +1,3 @@
-import yaml
-from pathlib import Path
 try:
     import torch
     from torchvision import transforms
@@ -8,40 +6,10 @@ except ImportError:
     exit(1)
 
 
-
-def get_device(): 
+def get_device():
     if torch.has_cuda:
-        return 'cuda'
-    if torch.has_mps: 
-        return torch.device('mps')
-        
-    return torch.device('cpu')
+        return "cuda"
+    if torch.has_mps:
+        return torch.device("mps")
 
-
-
-
-def rewrite_artifact_path(metadata_file, pwd, artifact_path_key):
-    with open(metadata_file, "r") as f:
-        y = yaml.safe_load(f)
-        y[artifact_path_key] = f"file://{pwd}"
-    
-    with open(metadata_file, "w") as f:
-        print(yaml.dump(y, default_flow_style=False, sort_keys=False))
-        yaml.dump(y, f, default_flow_style=False, sort_keys=False)
-
-
-def fix_mlflow_artifact_paths(mlflow_root: Path):
-    
-    for experiment_folder in mlflow_root.expanduser().resolve().iterdir():
-        metadata_file = experiment_folder / "meta.yaml"
-
-        # Fix experiment metadata
-        if metadata_file.exists():
-            rewrite_artifact_path(metadata_file, experiment_folder, artifact_path_key='artifact_location')
-        for run_folder in experiment_folder.iterdir():
-            metadata_file = run_folder / "meta.yaml"
-            print(run_folder)
-            
-            # Fix run metadata
-            if metadata_file.exists():
-                rewrite_artifact_path(metadata_file, run_folder / "artifacts", artifact_path_key='artifact_uri')
+    return torch.device("cpu")
