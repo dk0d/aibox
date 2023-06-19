cd458ffbf3307ad541d8656a1c6718a3e10cde91
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-06-19 16:18:02 -0400

    feat: add helpers for finding logged checkpoint artifacts in mlflow

--- src/aibox/utils.py
+++ src/aibox/utils.py
@@ -1,35 +1,116 @@
 from pathlib import Path
 from pprint import pformat
+from dataclasses import dataclass
 
 import yaml
 from rich import print as rprint
+
+
+@dataclass
+class MLFlowCheckpointEntry:
+    path: Path
+    metadata: dict
+    aliases: list[str]
+
+    def _get_metadata(self, k: str):
+        return self.metadata["Checkpoint"]["k"]
+
+    @property
+    def mode(self) -> list[Path]:
+        return self._get_metadata("mode")
+
+    @property
+    def monitor(self):
+        return self._get_metadata("monitor")
+
+    @property
+    def save_last(self):
+        return self._get_metadata("save_last")
+
+    @property
+    def save_top_k(self):
+        return self._get_metadata("save_top_k")
+
+    @property
+    def save_weights_only(self):
+        return self._get_metadata("save_weights_only")
+
+    @property
+    def original_filename(self):
+        return self.metadata["original_filename"]
+
+    @property
+    def score(self):
+        return self.metadata["score"]
+
+    @property
+    def name(self):
+        return self.path.parent.parent.name
+
+    @property
+    def run_dir(self) -> Path:
+        # assumes self.path is of the form: mlruns/<exp_id>/<run_id>/artifacts/model/checkpoints/<checkpoint_id>
+        return self.path.parent.parent.parent.parent.parent
+
+    @property
+    def run_meta_path(self) -> Path:
+        # assumes self.path is of the form: mlruns/<exp_id>/<run_id>/artifacts/model/checkpoints/<checkpoint_id>
+        return self.run_dir / "meta.yaml"
+
+    @property
+    def run_id(self):
+        # assumes path is of the form: mlruns/<exp_id>/<run_id>/artifacts/model/checkpoints/<checkpoint_id>
+        return self.run_dir.name
+
+    @property
+    def exp_id(self):
+        return self.run_dir.parent.name
 
 
 def rewrite_artifact_path(metadata_file, pwd, artifact_path_key):
     with open(metadata_file, "r") as f:
         y = yaml.safe_load(f)
         y[artifact_path_key] = f"file://{pwd}"
-    
+
     with open(metadata_file, "w") as f:
         print(yaml.dump(y, default_flow_style=False, sort_keys=False))
         yaml.dump(y, f, default_flow_style=False, sort_keys=False)
 
 
 def fix_mlflow_artifact_paths(mlflow_root: Path):
-    
     for experiment_folder in mlflow_root.expanduser().resolve().iterdir():
         metadata_file = experiment_folder / "meta.yaml"
 
         # Fix experiment metadata
         if metadata_file.exists():
-            rewrite_artifact_path(metadata_file, experiment_folder, artifact_path_key='artifact_location')
+            rewrite_artifact_path(metadata_file, experiment_folder, artifact_path_key="artifact_location")
         for run_folder in experiment_folder.iterdir():
             metadata_file = run_folder / "meta.yaml"
             print(run_folder)
-            
+
             # Fix run metadata
             if metadata_file.exists():
-                rewrite_artifact_path(metadata_file, run_folder / "artifacts", artifact_path_key='artifact_uri')
+                rewrite_artifact_path(metadata_file, run_folder / "artifacts", artifact_path_key="artifact_uri")
+
+
+def get_mlflow_checkpoints(mlruns_root: Path):
+    """
+    Returns a list of MLFlowCheckpointEntry objects
+
+    Assumes that mlruns_root is the root of the mlruns folder and that the checkpoints
+    were saved using the CombinedLogger
+    """
+    mlruns_root = mlruns_root.resolve()
+    checkpoints = [
+        MLFlowCheckpointEntry(
+            path=p,
+            metadata=yaml.load((p.parent / "metadata.yaml").read_text(), Loader=yaml.SafeLoader),
+            aliases=yaml.load((p.parent / "aliases.txt").read_text(), Loader=yaml.SafeLoader),
+        )
+        for p in mlruns_root.glob("**/*.ckpt")
+    ]
+    return checkpoints
+
 
 def print(*args, **kwargs):
     args = [a if isinstance(a, str) else pformat(a) for a in args]
