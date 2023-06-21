0cd93d137f77de7cb70d50db1d52f1e4e7096400
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-06-21 13:48:28 -0400

    feat: add mlflow helpers

--- src/aibox/utils.py
+++ src/aibox/utils.py
@@ -1,9 +1,67 @@
+from dataclasses import dataclass
 from pathlib import Path
 from pprint import pformat
-from dataclasses import dataclass
 
 import yaml
 from rich import print as rprint
+
+from .config import class_from_string, config_from_path
+
+try:
+    import mlflow
+
+    def search_runs(
+        run_name,
+        client: mlflow.MlflowClient,
+        experiment_ids: list[str] | None = None,
+        max_results=1,
+    ):
+        if experiment_ids is None:
+            experiment_ids = [e.experiment_id for e in client.search_experiments()]
+
+        runs = client.search_runs(
+            experiment_ids,
+            filter_string=f"attributes.run_name LIKE '%{run_name}%' ",
+            max_results=max_results,
+            order_by=["metrics.`test/loss` DESC"],
+        )
+
+        if len(runs) == 1:
+            return runs[0]
+
+        return runs
+
+    def load_model_from_run(run, tracking_uri, config, alias="best"):
+        try:
+            model_dir = mlflow.artifacts.download_artifacts(
+                run_id=run.info.run_id,
+                artifact_path="model",
+                tracking_uri=tracking_uri,
+            )
+            checkpoints = get_mlflow_checkpoints(Path(model_dir))
+            best = [c.path for c in checkpoints if alias in c.aliases][-1]
+            Model = class_from_string(config.model.class_path)
+            model = Model.load_from_checkpoint(best, **config.model.args)
+            return model
+        except Exception as e:
+            print(f"Error loading model for run ID: {run.info.run_id}")
+            print(e)
+
+    def load_config_from_run(run, tracking_uri, config_file="config.yml"):
+        try:
+            config_path = mlflow.artifacts.download_artifacts(
+                run_id=run.info.run_id,
+                artifact_path=config_file,
+                tracking_uri=tracking_uri,
+            )
+            config = config_from_path(config_path)
+            return config
+        except Exception as e:
+            print(f"Error loading config for run ID: {run.info.run_id}")
+            print(e)
+
+except ImportError:
+    pass
 
 
 @dataclass
