7760e27dfcbd382cc618d6d7c0a2bcac339a7d41
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-08-08 09:48:04 -0400

    add logger test

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,4 +1,13 @@
+# %%
+import torch
+import torchvision
+from skimage.color import label2rgb
+from torchvision.transforms import ToPILImage
+from aibox.torch.callbacks import LogImagesCallback
 from aibox.torch.logging import CombinedLogger
+from aibox.torch.image import display_images
+import PIL.Image as PIlImage
+import numpy as np
 import pytest
 
 DEFAULT_CONFIG_DIR = "tests/resources/configs"
@@ -6,3 +15,24 @@ DEFAULT_CONFIG_DIR = "tests/resources/configs"
 
 def test_cli_no_args():
     logger = CombinedLogger("./logs")
+
+
+def test_log_images_callback_interlace():
+    nImages = 7  # cols (number of images to interlace)
+    maxImages = 3  # rows (max images per batch to show)
+    images = [torch.ones((10, 3, 32, 32)) * i for i in range(1, nImages + 1)]
+    image = LogImagesCallback._interlace_images(images, maxImages=maxImages)
+    assert image.shape == (nImages * maxImages, 3, 32, 32), f"Expected shape (3*4, 3, 32, 32), got {image.shape}"
+    assert (
+        image[1].shape == images[1][0].shape
+    ), f"Expected first images shape to be same, got {image[0].shape} and {images[0][0].shape}"
+    assert torch.equal(image[1], images[1][0]), f"Expected image[1] == images[1], got {image[1]} != {images[1][0]}"
+
+    # grid = torchvision.utils.make_grid(image, nrow=nImages, padding=1)
+    # grid = np.array(ToPILImage()(grid).convert("L"))
+    # grid = (label2rgb(grid, grid) * 255).astype(np.uint8)
+    # grid = ToPILImage()(grid)
+    # display_images([grid])
+
+
+test_log_images_callback_interlace()
