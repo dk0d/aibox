482a93b8eb0431331b1f605ad355b584545a31f1
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-08-13 18:39:26 -0400

    update slurm testing modules

--- tests/test_slurm.py
+++ tests/test_slurm.py
@@ -1,4 +1,5 @@
 # %%
+import pytest
 import argparse
 from aibox.cli import AIBoxCLI
 from aibox.slurm.cli import main as submit_main
@@ -8,34 +9,30 @@ from aibox.utils import print
 DEFAULT_CONFIG_DIR = "tests/resources/configs"
 
 
-def test_slurm_cli():
-    args = [
-        "train",
-        "-e",
-        "debug",
-        "-cd",
-        DEFAULT_CONFIG_DIR,
-        "--model.name=TESTMODEL",
-        "--slurm.env_name=testenv",
-        "--debug",
-    ]
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "--debug",
-    #     "-d",
-    #     action="store_true",
-    #     default=False,
-    # )
-    # args, unknown = parser.parse_known_args(args)
-    # if len(unknown) % 2 != 0:
-    #     command, unknown = unknown[0], unknown[1:]
-    #     print(command, unknown)
-    # else:
-    #     command = None
-    #
-    # config = AIBoxCLI().parse_args(args=unknown)
-    # print(config)
+@pytest.mark.parametrize(
+    "args",
+    [
+        [
+            "train",
+            "-e",
+            "debug",
+            "-cd",
+            DEFAULT_CONFIG_DIR,
+            "--model.name=TESTMODEL",
+            "--slurm.env_name=testenv",
+            "--debug",
+        ],
+        [
+            "train",
+            "-e",
+            "debug-sl-modules",
+            "-cd",
+            DEFAULT_CONFIG_DIR,
+            "--model.name=TESTMODEL",
+            "--slurm.env_name=testenv",
+            "--debug",
+        ],
+    ],
+)
+def test_slurm_cli(args):
     submit_main(args)
-
-
-test_slurm_cli()
