266cf80e9d53558d1e739200ad0d086a3ff9165c
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2023-08-14 10:29:22 -0400

    chore: code cleanup

--- src/aibox/slurm/cli.py
+++ src/aibox/slurm/cli.py
@@ -1,62 +1,13 @@
 import argparse
-from aibox.cli import AIBoxCLI
-from aibox.slurm import SlurmConfig, submit_slurm_script
-
 import shutil
 
-# from aibox.config import config_merge
+from aibox.cli import AIBoxCLI
+from aibox.slurm import SlurmConfig, submit_slurm_script
 from aibox.utils import as_path, print
 
 
 def sbatch_available() -> bool:
     return shutil.which("sbatch") is not None
-
-
-#
-# class SlurmParser(AIBoxCLI):
-#     def __init__(self) -> None:
-#         super().__init__()
-#         self.parser.add_argument(
-#             "--slurm.env_name",
-#             "-slurm.env_name",
-#             help="name of conda environment to run in configs/environments",
-#             required=True,
-#         )
-#         self.parser.add_argument(
-#             "--slurm.env_dir",
-#             "-slurm.env_dir",
-#             help="path to the conda environments directory",
-#             action=PathAction,
-#             default=as_path("~/.conda/envs/"),
-#         )
-#         self.parser.add_argument(
-#             "--slurm.python_file",
-#             "-slurm.python_file",
-#             required=True,
-#             action=PathAction,
-#         )
-#
-#         self.parser.add_argument(
-#             "--slurm.script_dir",
-#             "-slurm.script_dir",
-#             default="slurm-scripts",
-#             action=PathAction,
-#         )
-#
-#     def parse_args(self, args=None):
-#         cli_config = self._parse_cli_args(args)
-#
-#         _, default_config = self._load_config(
-#             root=cli_config.exp_dir,
-#             name="default",
-#         )
-#
-#        _, experiment_config = self._resolve_config_from_root_name(root=cli_config.exp_dir, name=cli_config.exp_name)
-#
-#         # = Path("configs/experiments") / f"{cli_config.exp_name}.toml"
-#         config = config_merge(default_config, experiment_config, cli_config)
-#         config = self._resolve_links(config)
-#         return config
 
 
 def main(args=None):
@@ -85,7 +36,6 @@ def main(args=None):
     if args.debug:
         config.debug = True
 
-    # try:
     submit_slurm_script(
         name=config.name,
         env_name=config.slurm.env_name,
@@ -99,5 +49,3 @@ def main(args=None):
         conda_envs_dir=config.slurm.env_dir,
         debug=args.debug,
     )
-    # except Exception as err:
-    #     print(err)
